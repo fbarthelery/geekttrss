2658290d7dd5e16189064a7e18edf373e4893f66
Da Risk <da_risk@geekorum.com>
2018-12-03 17:40:27 -0800

    Fix license headers

--- app/src/androidTest/java/com/geekorum/ttrss/data/ArticleFullTextSearchTest.kt
+++ app/src/androidTest/java/com/geekorum/ttrss/data/ArticleFullTextSearchTest.kt
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Geekttrss.  If not, see <http:></http:>//www.gnu.org/licenses/>.
+ * along with Geekttrss.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.geekorum.ttrss.data
 
