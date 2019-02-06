490f3a8cce9b07f531f19c36a9d6100d80cfa4c5
Da Risk <da_risk@geekorum.com>
2019-02-06 08:01:08 -0800

    Dagger: add multiple modules for logging facilities
    TimberModules declares multibinding of Timber.Tree
    LogcatLoggingModule: provides a Timber.Tree to log to logcat
    CrashlyticsLoggingModule: provides a Timber.Tree to log to Crashlytics

---
+++ app/src/main/java/com/geekorum/ttrss/logging/TimberModule.kt
@@ -0,0 +1,35 @@
+/*
+ * Geekttrss is a RSS feed reader application on the Android Platform.
+ *
+ * Copyright (C) 2017-2018 by Frederic-Charles Barthelery.
+ *
+ * This file is part of Geekttrss.
+ *
+ * Geekttrss is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Geekttrss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Geekttrss.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.geekorum.ttrss.logging
+
+import dagger.Module
+import dagger.multibindings.Multibinds
+import timber.log.Timber
+
+/**
+ * Declare multibindings for [Timber.Tree]
+ */
+@Module
+abstract class TimberModule {
+
+    @Multibinds
+    abstract fun providesTimberTrees(): Set<Timber.Tree>
+}
