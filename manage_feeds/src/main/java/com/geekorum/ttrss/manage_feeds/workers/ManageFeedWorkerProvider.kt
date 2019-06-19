dae3d2d49dcfff57ad92530d6bd0247709e9d9c1
Da Risk <da_risk@geekorum.com>
2019-06-18 17:31:34 -0700

    manage_feeds: Add WorkerFactoryProvider implementation

---
+++ manage_feeds/src/main/java/com/geekorum/ttrss/manage_feeds/workers/ManageFeedWorkerProvider.kt
@@ -0,0 +1,37 @@
+/*
+ * Geekttrss is a RSS feed reader application on the Android Platform.
+ *
+ * Copyright (C) 2017-2019 by Frederic-Charles Barthelery.
+ *
+ * This file is part of Geekttrss.
+ *
+ * Geekttrss is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Geekttrss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Geekttrss.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.geekorum.ttrss.manage_feeds.workers
+
+import androidx.annotation.Keep
+import androidx.work.WorkerFactory
+import com.geekorum.ttrss.di.ApplicationComponent
+import com.geekorum.ttrss.features_api.WorkerFactoryProvider
+import com.geekorum.ttrss.manage_feeds.DaggerManageFeedComponent
+
+@Keep
+class ManageFeedWorkersProvider : WorkerFactoryProvider {
+    override fun getWorkerFactories(appComponent: ApplicationComponent): List<WorkerFactory> {
+        val manageFeedComponent = DaggerManageFeedComponent.builder()
+            .manageFeedsDependencies(appComponent)
+            .build()
+        return listOf(manageFeedComponent.getWorkerFactory())
+    }
+}
