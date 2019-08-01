9e367a6c21418e6e7cbe1daacae4cb3036c4be2b
Da Risk <da_risk@geekorum.com>
2019-08-01 15:30:17 -0700

    Add an api for an InAppUpdateManager

---
+++ app/src/main/java/com/geekorum/ttrss/in_app_update/InAppUpdateManager.kt
@@ -0,0 +1,62 @@
+/*
+ * Geekttrss is a RSS feed reader application on the Android Platform.
+ *
+ * Copyright (C) 2017-2019 by Frederic-Charles Barthelery.
+ *
+ * This file is part of Geekttrss.
+ *
+ * Geekttrss is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Geekttrss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Geekttrss.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.geekorum.ttrss.in_app_update
+
+import android.app.Activity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Allows to query some service to know if an Application update is available,
+ * and if so, to download and install it.
+ */
+@UseExperimental(ExperimentalCoroutinesApi::class)
+interface InAppUpdateManager {
+
+    suspend fun getUpdateAvailability(): UpdateAvailability
+
+    /**
+     * Start the update process.
+     */
+    suspend fun startUpdate(activity: Activity, requestCode: Int): Flow<UpdateState>
+
+    /**
+     * If the [UpdateState.status] is [UpdateState.Status.DOWNLOADED], this method need to be called
+     * complete the install and restart the application
+     */
+    fun completeUpdate()
+
+}
+
+enum class UpdateAvailability {
+    UPDATE_AVAILABLE, NO_UPDATE
+}
+
+data class UpdateState(
+    val status: Status,
+    val errorCode: Any? = null
+) {
+    enum class Status {
+        UNKNOWN, PENDING, DOWNLOADING,
+        DOWNLOADED, INSTALLING, INSTALLED,
+        FAILED, CANCELED
+    }
+}
---
+++ app/src/main/java/com/geekorum/ttrss/in_app_update/NoInAppUpdateManager.kt
@@ -0,0 +1,53 @@
+/*
+ * Geekttrss is a RSS feed reader application on the Android Platform.
+ *
+ * Copyright (C) 2017-2019 by Frederic-Charles Barthelery.
+ *
+ * This file is part of Geekttrss.
+ *
+ * Geekttrss is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Geekttrss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Geekttrss.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.geekorum.ttrss.in_app_update
+
+import android.app.Activity
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+@UseExperimental(ExperimentalCoroutinesApi::class)
+private class NoInAppUpdateManager: InAppUpdateManager {
+
+    override suspend fun getUpdateAvailability(): UpdateAvailability {
+        return UpdateAvailability.NO_UPDATE
+    }
+
+    override suspend fun startUpdate(activity: Activity, requestCode: Int): Flow<UpdateState> {
+        return flowOf(UpdateState(UpdateState.Status.FAILED))
+    }
+
+    override fun completeUpdate() {
+        // do nothing
+    }
+}
+
+@Module
+object NoInAppUpdateModule {
+
+    @Provides
+    @JvmStatic
+    fun providesInAppUpdateManager(): InAppUpdateManager = NoInAppUpdateManager()
+
+}
