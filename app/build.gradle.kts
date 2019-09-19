73af38b390148efed2f0766cd56fe9242cd0583d
Da Risk <da_risk@geekorum.com>
2019-09-18 17:14:43 -0700

    build: update google play core library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -191,7 +191,7 @@ dependencies {
     // ensure that the free flavor don't get any firebase dependencies
     configurations["freeImplementation"].exclude(group = "com.google.firebase")
 
-    add("googleImplementation", "com.google.android.play:core:1.5.0")
+    add("googleImplementation", "com.google.android.play:core:1.6.1")
 
     // api dependencies for features modules
     api("androidx.appcompat:appcompat:1.1.0")
