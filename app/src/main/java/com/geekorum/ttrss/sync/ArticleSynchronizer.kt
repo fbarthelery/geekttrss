3a443473f18ac242f528a79c12d28027177a573c
Da Risk <da_risk@geekorum.com>
2019-11-10 13:16:48 -0800

    ArticleSynchronizer: only update status for selected feed

--- app/src/main/java/com/geekorum/ttrss/sync/ArticleSynchronizer.kt
+++ app/src/main/java/com/geekorum/ttrss/sync/ArticleSynchronizer.kt
@@ -177,19 +177,22 @@ class ArticleSynchronizer @AssistedInject constructor(
                 .build()
 
         val tag = UUID.randomUUID().toString()
-        val jobRequests = databaseService.getFeeds().map { feed ->
-            val inputData = workDataOf(
-                    SyncWorkerFactory.PARAM_ACCOUNT_NAME to account.name,
-                    SyncWorkerFactory.PARAM_ACCOUNT_TYPE to account.type,
-                    UpdateArticleStatusWorker.PARAM_FEED_ID to feed.id,
-                    UpdateArticleStatusWorker.PARAM_NUMBER_OF_LATEST_ARTICLES_TO_REFRESH to numberOfLatestArticlesToRefresh
-            )
-            OneTimeWorkRequestBuilder<UpdateArticleStatusWorker>()
-                    .setConstraints(constraints)
-                    .setInputData(inputData)
-                    .addTag(tag)
-                    .build()
-        }
+        val jobRequests = databaseService.getFeeds()
+                .filter {
+                    it.id == feedId || feedId == ApiService.ALL_ARTICLES_FEED_ID
+                }.map { feed ->
+                    val inputData = workDataOf(
+                            SyncWorkerFactory.PARAM_ACCOUNT_NAME to account.name,
+                            SyncWorkerFactory.PARAM_ACCOUNT_TYPE to account.type,
+                            UpdateArticleStatusWorker.PARAM_FEED_ID to feed.id,
+                            UpdateArticleStatusWorker.PARAM_NUMBER_OF_LATEST_ARTICLES_TO_REFRESH to numberOfLatestArticlesToRefresh
+                    )
+                    OneTimeWorkRequestBuilder<UpdateArticleStatusWorker>()
+                            .setConstraints(constraints)
+                            .setInputData(inputData)
+                            .addTag(tag)
+                            .build()
+                }
         updateStatusJobsTag = tag
 
         workManager.enqueue(jobRequests).await()
