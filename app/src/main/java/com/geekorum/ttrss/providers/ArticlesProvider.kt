89b2e4dfc5a8fa24c1f4f2871decc2aa43906553
Da Risk <da_risk@geekorum.com>
2019-11-26 21:09:32 -0800

    Restore ArticlesProvider for android synchronization framework

---
+++ app/src/main/java/com/geekorum/ttrss/providers/ArticlesProvider.kt
@@ -0,0 +1,47 @@
+/*
+ * Geekttrss is a RSS feed reader application on the Android Platform.
+ *
+ * Copyright (C) 2017-2019 by Frederic-Charles Barthelery.
+ *
+ * This file is part of Geekttrss.
+ *
+ * Geekttrss is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Geekttrss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Geekttrss.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.geekorum.ttrss.providers
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+/**
+ * [ContentProvider] for articles.
+ *
+ * This implementation doesn't support any operations.
+ * It's only there to work be able to use [com.geekorum.ttrss.sync.ArticleSyncAdapter] with the
+ * ContentProvider synchronization framework
+ */
+class ArticlesProvider : ContentProvider() {
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? = null
+
+    override fun onCreate(): Boolean = true
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int = 0
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
+
+    override fun getType(uri: Uri): String? = null
+}
