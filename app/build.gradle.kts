4ca83e3f86e32693002c79083bcf60de82559557
Da Risk <da_risk@geekorum.com>
2020-03-30 23:27:18 -0400

    Update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,7 +117,7 @@ kapt {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.2.0")
-    implementation("androidx.fragment:fragment-ktx:1.2.1")
+    implementation("androidx.fragment:fragment-ktx:1.2.3")
     implementation("androidx.activity:activity-ktx:1.1.0")
 
     // androidx ui
