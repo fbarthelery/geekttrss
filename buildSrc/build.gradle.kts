aef82b7e1aca51b64909210ea5f7de07606d9eea
Da Risk <da_risk@geekorum.com>
2021-05-19 15:04:35 -0400

    build: update to latest license checker plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -39,7 +39,7 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.2.1")
-    implementation("gradle.plugin.com.hierynomus.gradle.plugins:license-gradle-plugin:0.15.0")
+    implementation("gradle.plugin.com.hierynomus.gradle.plugins:license-gradle-plugin:0.16.1")
     implementation("com.github.triplet.gradle:play-publisher:2.7.5")
     implementation("com.geekorum.gradle.avdl:flydroid:0.0.2")
 }
