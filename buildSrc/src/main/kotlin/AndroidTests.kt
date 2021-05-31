3633665037d8be97a8f16893561d042fad69c3c4
Da Risk <da_risk@geekorum.com>
2021-05-31 14:49:52 -0400

    build: update mockk

--- buildSrc/src/main/kotlin/AndroidTests.kt
+++ buildSrc/src/main/kotlin/AndroidTests.kt
@@ -85,8 +85,8 @@ internal fun Project.configureTests() {
         dualTestImplementation("androidx.test.ext:truth:1.3.0-alpha01")
 
         // mock
-        testImplementation("io.mockk:mockk:1.10.3")
-        androidTestImplementation("io.mockk:mockk-android:1.10.3")
+        testImplementation("io.mockk:mockk:1.11.0")
+        androidTestImplementation("io.mockk:mockk-android:1.11.0")
         testImplementation("org.robolectric:robolectric:$robolectricVersion")
 
         constraints {
