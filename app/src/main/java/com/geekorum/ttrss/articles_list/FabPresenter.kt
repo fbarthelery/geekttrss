860539f6c1290354417fd13035bd0888d0e8c4df
Da Risk <da_risk@geekorum.com>
2023-03-21 19:31:41 -0400

    app: rework FabPresenter for full compose

--- app/src/main/java/com/geekorum/ttrss/articles_list/FabPresenter.kt
+++ app/src/main/java/com/geekorum/ttrss/articles_list/FabPresenter.kt
@@ -20,48 +20,81 @@
  */
 package com.geekorum.ttrss.articles_list
 
-import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.geekorum.geekdroid.views.behaviors.ScrollAwareFABBehavior
+import androidx.navigation.NavDestination
 import com.geekorum.ttrss.R
 import com.geekorum.ttrss.on_demand_modules.OnDemandModuleNavHostProgressDestinationProvider
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * Controls the behavior of the FloatingActionButton
  */
 internal class FabPresenter(
-    private val fab: FloatingActionButton,
-    private val onDemandModuleNavHostProgressDestinationProvider: OnDemandModuleNavHostProgressDestinationProvider,
+    private val onDemandModuleNavHostProgressDestinationProvider: OnDemandModuleNavHostProgressDestinationProvider?,
     private val navController: NavController
 ){
 
-    private val fabBehavior: ScrollAwareFABBehavior? =
-        (fab.layoutParams as? CoordinatorLayout.LayoutParams)?.behavior as? ScrollAwareFABBehavior
+    @OptIn(ExperimentalAnimationApi::class)
+    @Composable
+    fun Content(isScrollingUpOrRest: Boolean, onClick: () -> Unit, modifier: Modifier = Modifier) {
+        //TODO deal with module install
+        val navBackStackEntry by navController.currentBackStackEntryFlow.collectAsStateWithLifecycle(null)
+        val progressDestinationId = onDemandModuleNavHostProgressDestinationProvider?.progressDestinationId ?: 0
+        val fabVisibleInDestination = when {
+            navBackStackEntry?.destination?.route == NavRoutes.Search -> false
+            navBackStackEntry?.destination?.id == progressDestinationId -> false
+            navBackStackEntry == null -> false
+            else -> true
+        }
 
-    init {
-        setup()
-    }
+        val fabVisible = fabVisibleInDestination && isScrollingUpOrRest
 
-    private fun setup() {
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            val progressDestinationId = onDemandModuleNavHostProgressDestinationProvider.progressDestinationId
-            when (destination.id) {
-                progressDestinationId,
-                R.id.articlesSearchFragment -> {
-                    setFabBehavior(null)
-                    fab.hide()
-                }
-                else -> {
-                    setFabBehavior(fabBehavior)
-                    fab.show()
+        val targetState = if (fabVisible) {
+            val feedId = if (navBackStackEntry?.destination?.route == NavRoutes.ArticlesList) {
+                navBackStackEntry?.arguments?.getLong("feed_id")
+            } else null
+            DestinationWithFeedId(
+                destination = navBackStackEntry?.destination,
+                feedId = feedId
+            )
+        } else null
+        AnimatedContent(
+            modifier = modifier,
+            targetState = targetState,
+            transitionSpec = {
+                scaleIn(tween(300, delayMillis = 100)) with
+                        scaleOut(tween(300)) using
+                        SizeTransform(clip = false) { _, _ ->
+                            // delay size transform to make scaleOut happens in place
+                            tween(300, delayMillis = if (targetState == null) 300 else 0)
+                        }
+            },
+            contentAlignment = Alignment.Center,
+        ) { state ->
+            if (state != null) {
+                FloatingActionButton(
+                    modifier = Modifier.navigationBarsPadding(),
+                    onClick = onClick
+                ) {
+                    Icon(Icons.Default.Refresh, stringResource(R.string.btn_refresh))
                 }
             }
         }
     }
 
-    private fun setFabBehavior(behavior: ScrollAwareFABBehavior?) {
-        (fab.layoutParams as? CoordinatorLayout.LayoutParams)?.behavior = behavior
-    }
-
+    private data class DestinationWithFeedId(
+        val destination: NavDestination?,
+        val feedId: Long?
+    )
 }
