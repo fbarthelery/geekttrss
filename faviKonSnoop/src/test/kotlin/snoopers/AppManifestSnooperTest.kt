74551cffe1fbf2f75b65d66389b81bfeebd35a0b
Da Risk <da_risk@geekorum.com>
2023-12-07 19:16:16 -0400

    Backed out changeset 1cb520ca05cc

--- faviKonSnoop/src/test/kotlin/snoopers/AppManifestSnooperTest.kt
+++ faviKonSnoop/src/test/kotlin/snoopers/AppManifestSnooperTest.kt
@@ -35,7 +35,6 @@ import okhttp3.Response
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Test
 import kotlin.test.BeforeTest
-import kotlin.test.Ignore
 
 private val SIMPLE_MANIFEST = """
 {
@@ -143,7 +142,6 @@ class AppManifestSnooperTest {
         assertThat(result).isEmpty()
     }
 
-    @Ignore("works fine in local but hang up on CI")
     @Test
     fun testHtmlWithInvalidManifestReturnsEmpty() = testScope.runTest {
         mockkStatic("com.geekorum.favikonsnoop.FaviKonSnoopKt")
