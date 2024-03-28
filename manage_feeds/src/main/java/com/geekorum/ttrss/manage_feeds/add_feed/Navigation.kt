df7e1d06090e591130a5ce4b60968a077f647061
Da Risk <da_risk@geekorum.com>
2024-03-28 16:35:33 -0400

    manage_feeds: tweak transitions of add_feed navhost

--- manage_feeds/src/main/java/com/geekorum/ttrss/manage_feeds/add_feed/Navigation.kt
+++ manage_feeds/src/main/java/com/geekorum/ttrss/manage_feeds/add_feed/Navigation.kt
@@ -20,8 +20,19 @@
  */
 package com.geekorum.ttrss.manage_feeds.add_feed
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection
+import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -31,6 +42,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.geekorum.ttrss.manage_feeds.R
+import kotlin.math.sign
 
 val ROUTE_ENTER_FEED_URL = "enter_feed_url"
 val ROUTE_SELECT_FEED = "select_feed"
@@ -43,9 +55,28 @@ fun SubscribeToFeedNavHost(
     navController: NavHostController = rememberNavController(),
     finishActivity: () -> Unit,
 ) {
-    NavHost(modifier = modifier, navController = navController, startDestination = ROUTE_ENTER_FEED_URL) {
-        composable(ROUTE_ENTER_FEED_URL) {
-            // TODO animation with navigation 1.7.x
+    val slideOffsetPx = with(LocalDensity.current) {
+        30.dp.roundToPx()
+    }
+    NavHost(modifier = modifier, navController = navController, startDestination = ROUTE_ENTER_FEED_URL,
+        contentAlignment = Alignment.TopStart,
+        enterTransition = { sharedAxisXEnterTransition(SlideDirection.Start, slideOffsetPx) },
+        exitTransition = { sharedAxisXExitTransition(SlideDirection.Start, slideOffsetPx) },
+        popEnterTransition = { sharedAxisXEnterTransition(SlideDirection.End, slideOffsetPx) },
+        popExitTransition = { sharedAxisXExitTransition(SlideDirection.End, slideOffsetPx) },
+    ) {
+        composable(ROUTE_ENTER_FEED_URL,
+            enterTransition = {
+                if (initialState.destination.route == ROUTE_DISPLAY_ERROR) {
+                    fadeIn(animationSpec = tween(700))
+                } else null
+            },
+            exitTransition = {
+                if (targetState.destination.route == ROUTE_DISPLAY_ERROR) {
+                    fadeOut(animationSpec = tween(700))
+                } else null
+            }
+        ) {
             EnterFeedUrlScreen(viewModel,
                 navigateToDisplayError = {
                     navController.navigateToDisplayError(it)
@@ -58,17 +89,53 @@ fun SubscribeToFeedNavHost(
         composable(ROUTE_SELECT_FEED) {
             SelectFeedScreen(viewModel)
         }
-        composable(ROUTE_DISPLAY_ERROR, arguments = listOf(
-            navArgument("errorMsgId") {
-                type = NavType.IntType
-                defaultValue = R.string.fragment_display_error_no_feeds_found
-            }
-        )) {
+        composable(ROUTE_DISPLAY_ERROR,
+            enterTransition = { fadeIn(animationSpec = tween(700)) },
+            exitTransition = { fadeOut(animationSpec = tween(700)) },
+            arguments = listOf(
+                navArgument("errorMsgId") {
+                    type = NavType.IntType
+                    defaultValue = R.string.fragment_display_error_no_feeds_found
+                }
+            )
+        ) {
             val errorMsgId = it.arguments!!.getInt("errorMsgId")
             DisplayErrorScreen(errorMsgId = errorMsgId)
         }
     }
 }
+
+/*
+ try to replicate material motion spec for shared axis X
+ https://material.io/design/motion/the-motion-system.html#shared-axis
+ */
+private fun <S> AnimatedContentTransitionScope<S>.sharedAxisXEnterTransition(
+    towards: SlideDirection,
+    slideOffsetPx: Int
+) = slideIntoContainer(
+    towards = towards,
+    initialOffset = {
+        // keep sign of full slide offset
+        it.sign * slideOffsetPx
+    },
+    animationSpec = tween(AnimationConstants.DefaultDurationMillis)) +
+        fadeIn(animationSpec = keyframes {
+            delayMillis = 90
+            durationMillis = AnimationConstants.DefaultDurationMillis - 90
+            0f at 0 using CubicBezierEasing(0.4f, 0.0f, 0.2f, 1.0f)
+        })
+
+private fun <S> AnimatedContentTransitionScope<S>.sharedAxisXExitTransition(
+    towards: SlideDirection,
+    slideOffsetPx: Int
+) = slideOutOfContainer(
+    towards = towards,
+    targetOffset = { it.sign * slideOffsetPx },
+    animationSpec = tween(AnimationConstants.DefaultDurationMillis)) +
+            fadeOut(animationSpec = keyframes {
+                durationMillis = 90
+                0f at 0 using CubicBezierEasing(0.4f, 0.0f, 0.2f, 1.0f)
+            })
 
 private fun NavController.navigateToDisplayError(errorMsgId: Int?) {
     val route = buildString {
