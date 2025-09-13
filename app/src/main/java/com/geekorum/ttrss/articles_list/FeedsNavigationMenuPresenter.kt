2d987506be10e4c3229d0bfd8dc1b12f293a8fff
Da Risk <da_risk@geekorum.com>
2025-09-13 15:58:57 -0400

    app: FeedsNavigationMenuPresenter make ManageFeeds container Transparent to use background of drawer

--- app/src/main/java/com/geekorum/ttrss/articles_list/FeedsNavigationMenuPresenter.kt
+++ app/src/main/java/com/geekorum/ttrss/articles_list/FeedsNavigationMenuPresenter.kt
@@ -22,23 +22,36 @@ package com.geekorum.ttrss.articles_list
 
 import android.app.Activity
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Tune
-import androidx.compose.material3.*
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
@@ -202,7 +215,10 @@ private fun ManageFeedSection(
     progressMax: Int,
     onItemClick: () -> Unit,
 ) {
-    Surface(onClick = onItemClick, modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)) {
+    Surface(onClick = onItemClick,
+        color = Color.Transparent,
+        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+    ) {
         Column(Modifier.animateContentSize()) {
             ManageFeedItem()
 
