88eb38b293c028671b2b3744ac57abedd06dd08d
Da Risk <da_risk@geekorum.com>
2026-02-23 16:04:29 -0400

    build: use gradle-daemon-jvm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,7 +37,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 include(":app")
